62b4af6f7f6b02c257ab651ca5a896d586945e14
youyeon <yourain416@gmail.com>
2020-04-12 14:08:34 +0900

    Problem 1920 수 찾기

---
+++ 1000~9999/1920.cpp
@@ -0,0 +1,33 @@
+// https://www.acmicpc.net/problem/1920
+// 제목: 수 찾기
+#include <stdio.h>
+#include <algorithm>
+int find(int,int);
+int A[100000];
+
+int main(void) {
+	int N, M, K;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &A[i]);
+	std::sort(A, A + N);
+	scanf("%d", &M);
+	for (int i = 0; i < M; i++) {
+		scanf("%d", &K);
+		printf("%d\n",find(K,N));
+	}
+}
+
+int find(int K, int N) {
+	int low = 0, high=N;
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (K == A[mid])
+			return 1;
+		else if (K < A[mid])
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return (int)0;
+}
