45c9df87aa14e030767aceac484dcc2e9d018e54
Youyeon <58042454+Youyeon@users.noreply.github.com>
2021-03-10 00:00:35 +0900

    Update 19-13-1.cpp
    주석 해제

--- epper/19-13-1.cpp
+++ epper/19-13-1.cpp
@@ -1,16 +1,16 @@
-//// 19³âµµ 13È¸ 1¹ø
-//#include <iostream>
-//using namespace std;
-//
-//int main(void) {
-//	int sum = 0;
-//	int n;
-//	cin >> n;
-//	for (int i = 0; i < n; i++) {
-//		int num;
-//		cin >> num;
-//		if (num % 2)
-//			sum += num;
-//	}
-//	cout << sum;
-//}
+// 19ë…„ 13íšŒ 1ë²ˆ
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int sum = 0;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		if (num % 2)
+			sum += num;
+	}
+	cout << sum;
+}
